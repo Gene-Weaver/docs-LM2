d30ccf2d8ef53c3799fcff5d8f4a7034bbd2d014
Gene-Weaver <willwe@umich.edu>
2024-05-06 17:01:47 -0400

    change path

--- source/conf.py
+++ source/conf.py
@@ -2,4 +2,4 @@ html_theme = 'pydata_sphinx_theme'
 html_theme_options = {
     'navigation_with_keys': True,  
 }
-exclude_patterns = ['_build', '.venv']
+exclude_patterns = ['_build', '.venv','docs']
