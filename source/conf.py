93b9158314132466aefdd8329a26f1eef6d4ae0a
Mark Pitblado <116753126+mpitblado@users.noreply.github.com>
2024-05-07 13:54:05 -0700

    Add title and include sphinx-design n extensions

--- source/conf.py
+++ source/conf.py
@@ -1,5 +1,9 @@
-html_theme = 'pydata_sphinx_theme'
+html_theme = "pydata_sphinx_theme"
 html_theme_options = {
-    'navigation_with_keys': True,  
+    "logo": {
+        "text": "Leafmachine2",
+    },
+    "navigation_with_keys": True,
 }
-exclude_patterns = ['_build', '.venv','docs']
+exclude_patterns = ["_build", ".venv", "docs"]
+extensions = ["sphinx_design"]
